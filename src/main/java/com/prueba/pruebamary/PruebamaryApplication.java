f1ccc975dab0f9d9c548efae328fe7a27a17785e
Marytecnonic <marytecnonic@gmail.com>
2022-11-11 17:03:31 -0600

    segundo commit

---
+++ src/main/java/com/prueba/pruebamary/PruebamaryApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.pruebamary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebamaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebamaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/pruebamary/PruebamaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.pruebamary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebamaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
